12e2b2e10fd73977ad71d0fc58dfa58cb2916065
Firsta Adi Pradana <67580508+firstaadi-dev@users.noreply.github.com>
2021-09-30 14:39:19 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import XLSX from "xlsx";
+
+function App() {
+  const [rows, setRows] = useState();
+
+  async function handleUpload(e) {
+    e.preventDefault();
+    const targetFile = e.target.files[0];
+    const buffer = await targetFile.arrayBuffer();
+    const imported = XLSX.read(buffer, { type: "buffer" });
+    const sheet = imported.Sheets[imported.SheetNames[0]];
+    const json = XLSX.utils.sheet_to_json(sheet);
+    setRows(json);
+  }
+
+  return (
+    <div>
+      <h3>Input file .xlsx</h3>
+      <input
+        type="file"
+        onChange={handleUpload}
+        accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      />
+      {rows && (
+        <div>
+          <h3>Result : </h3>
+          <div>
+            <a
+              href={`data:text/json;charset=utf-8,${encodeURIComponent(
+                JSON.stringify(rows)
+              )}`}
+              download="converted.json"
+            >
+              Download Json
+            </a>
+            <div>{JSON.stringify(rows)}</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
